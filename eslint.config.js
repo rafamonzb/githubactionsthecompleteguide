0a1def1dd0e68796e25be151e7a6f6a63f388f94
rafamonzb <rafael.monzo-bravo@capgemini.com>
2025-10-02 10:59:50 +0200

    Change sourcetype to module

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { files: ["**/*.test.*", "**/*.spec.*"], languageOptions: {
     globals: {
       ...globals.jest,
