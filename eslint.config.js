0930d7afb9e51af7ef6a9e2baaf4759c1d8cedcf
rafamonzb <rafael.monzo-bravo@capgemini.com>
2025-10-02 11:03:48 +0200

    Revert "Redo some things involving commonjs"
    This reverts commit 5b14cf49d94dc7c6a8e4e1d36346f1063161c7b1.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+// import js from "@eslint/js";
+const js = require('@eslint/js');
+const globals = require('globals');
+// const defineConfig = require('eslint/config');
+// import globals from "globals";
+// import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+module.exports = [
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.test.*", "**/*.spec.*"], languageOptions: {
     globals: {
@@ -11,4 +14,4 @@ export default defineConfig([
       ...globals.browser,
     }
   }}
-]);
+];
