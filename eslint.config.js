104c3f9a67cc2b74c7d47acb10678cc2d0e2ea54
rafamonzb <rafael.monzo-bravo@capgemini.com>
2025-10-02 10:35:22 +0200

    Add a rule to evade linting tests

--- eslint.config.js
+++ eslint.config.js
@@ -5,4 +5,10 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.test.*", "**/*.spec.*"], languageOptions: {
+    globals: {
+      ...globals.jest,
+      ...globals.browser,
+    }
+  }}
 ]);
